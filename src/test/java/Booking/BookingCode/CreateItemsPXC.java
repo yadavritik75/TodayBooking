fcee635d3904be1fc41d4cd2ccb25cfa7ec9fa91
ritik <honeyritikjob75@gmail.com>
2025-09-01 12:11:01 +0530

    1sep commited

--- src/test/java/Booking/BookingCode/CreateItemsPXC.java
+++ src/test/java/Booking/BookingCode/CreateItemsPXC.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+//@@@@@@@@@@@@@@@@@@@@Testing
 public class CreateItemsPXC {
 public static void main(String[] args) throws InterruptedException {
 	 WebDriver driver = new ChromeDriver();
